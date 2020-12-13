5d9810918155021acc222a28c6318fa26f472d59
Li Hao <james_lai@ntucenterprise.sg>
2020-12-13 21:14:20 +0800

    wip: movie discover api

--- Moviz/ViewController.swift
+++ Moviz/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
+    private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        let service = DiscoverService()
+        service.movie()
+            .subscribe()
+            .disposed(by: disposeBag)
+
+        layout()
     }
 
-
+    private func layout() {
+        title = "Discover"
+        view.backgroundColor = .white
+    }
 }
 
